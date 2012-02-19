efd27d288fbb8e9cd1a41a52866bce5d26e6a99f
Shelly McGowan <smcgowan@redhat.com>
2012-02-18 22:55:42 -0500

    JBPAPP-6738 add standalone-full-test.xml to include jms-destinations, update basic and smoke test poms to use it

--- testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/demos/jms/JmsTestCase.java
+++ testsuite/integration/smoke/src/test/java/org/jboss/as/test/smoke/embedded/demos/jms/JmsTestCase.java
@@ -46,7 +46,6 @@ import org.jboss.shrinkwrap.api.ShrinkWrap;
 import org.jboss.shrinkwrap.api.spec.JavaArchive;
 import org.junit.After;
 import org.junit.Before;
-import org.junit.Ignore;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 
@@ -56,7 +55,6 @@ import org.junit.runner.RunWith;
  * @author <a href="kabir.khan@jboss.com">Kabir Khan</a>
  */
 @RunWith(Arquillian.class)
-@Ignore("https://issues.jboss.org/browse/JBPAPP-6738")
 public class JmsTestCase {
     static final Logger log = Logger.getLogger(JmsTestCase.class);
 
